f1c767ede1aced1b727f903e04d3f75bdf59583e
Anna Bakulina <bakulina.a@yandex.ru>
2022-12-15 23:21:40 +0500

    added /build/install/app/bin/app

--- app/src/main/java/hexlet/code/app.java
+++ app/src/main/java/hexlet/code/app.java
@@ -2,6 +2,6 @@ package hexlet.code;
 
 public class app {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to the Brain Games!");
     }
 }
