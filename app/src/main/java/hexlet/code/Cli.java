06e51852ff91131811476b12b83f44c75cf626a5
Anna Bakulina <bakulina.a@yandex.ru>
2022-12-16 23:10:46 +0500

    fixed style errors

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,7 +2,7 @@ package hexlet.code;
 
 import java.util.Scanner;
 public class Cli {
-    public static void greeting(){
+    public static void greeting() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name");
